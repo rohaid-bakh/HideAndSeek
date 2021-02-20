aa203ee0e43dc196dd3d610604950999ef228f17
unknown <rohaidb1999@gmail.com>
2021-02-20 16:57:43 -0500

    partial player movement

---
+++ HideAndSeek/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float zaxis = Input.GetAxis("Vertical");
+       gameObject.transform.position = new Vector3 (transform.position.x + (horizontal  * 4.0f), 
+      transform.position.y , transform.position.z + (zaxis *4.0f));
+        
+    }
+}
