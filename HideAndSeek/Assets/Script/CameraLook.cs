24f5a11aeb914a44065135a8b7e0b75e60863828
unknown <rohaidb1999@gmail.com>
2021-02-20 17:11:08 -0500

    implemented player movement/ camera movement

--- HideAndSeek/Assets/Script/CameraLook.cs
+++ HideAndSeek/Assets/Script/CameraLook.cs
@@ -12,7 +12,7 @@ public class CameraLook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = Cursor.LockMode.Locked; 
+        // Cursor.lockState = CursorLockMode.Locked; 
     }
 
     // Update is called once per frame
@@ -22,7 +22,7 @@ public class CameraLook : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
          xRotation -= mouseY;
          xRotation = Mathf.Clamp(xRotation, -90f , 90f);
-         Transform localRotation = Quaternion.Euler(xRotation , 0f , 0f);
+         transform.localRotation = Quaternion.Euler(xRotation , 0f , 0f);
         playerBody.Rotate(Vector3.up * mouseX);
        
     }
