bc708949e178ff6bb9deae8c2dbf8f43c01ce8d7
winterline <niwanoleiko@gmail.com>
2021-02-20 21:24:16 -0500

    Added Grid.cs and Node.cs

---
+++ HideAndSeek/Assets/Script/Grid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public LayerMask unwalkableMask;
+    public Vector2 gridWS;
+    public float nodeRadius;
+    Node[,] grid;
+
+    void OnDrawGizmos
+}
---
+++ HideAndSeek/Assets/Script/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool walk, Vector3 worldPos)
+    {
+        walkalbe = walk;
+        worldPosition = worldPos;
+    }
+}
